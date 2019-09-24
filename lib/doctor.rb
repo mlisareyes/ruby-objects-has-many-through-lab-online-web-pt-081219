992dc8461e6adb5fc4482ddddcee3779594af34b
Monica R <mlisareyes@gmail.com>
2019-09-24 04:17:15 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -18,7 +18,7 @@ class Doctor
   end
   
   def appointments
-    Appointment.all.select { |appointment| appointment.patient == self}
+    Appointment.all.select { |appointment| appointment.doctor == self}
   end
   
   def patients
