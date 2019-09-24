7aba54bb070d2d463d18b687ac44d3032c60960a
Monica R <mlisareyes@gmail.com>
2019-09-24 04:25:42 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,17 +1,3 @@
 class Appointment
-  
-  attr_accessor :date, :patient, :doctor
-  
-  @@all = []
-  
-  def initialize(patient, doctor, date)
-    @patient = patient
-    @doctor = doctor
-    @date = date
-    @@all << self
-  end
-  
-  def self.all
-    @@all 
-  end
+
 end
